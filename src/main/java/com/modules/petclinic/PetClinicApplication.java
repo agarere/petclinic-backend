b9b0258afa205fa944e71aa550c2da4ab82d40f7
EdizAgarer <edizagarer@gmail.com>
2021-10-21 01:40:03 +0300

    to test

--- src/main/java/com/modules/petclinic/PetClinicApplication.java
+++ src/main/java/com/modules/petclinic/PetClinicApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
